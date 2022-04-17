ede723e745989ec8f65582cd6a3075e690aac11a
John <jjnrmason@me.com>
2022-04-17 14:27:28 +0100

    Start of new Website design

--- bakery/index.php
+++ bakery/index.php
@@ -1,32 +1,81 @@
-<?php 
-    include"./includes/header.php"
-?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masonry Bakery</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+    <link rel="stylesheet" href="./css/main.css"/>
+</head>
+<body>
+    <div class="home-header">
+        <nav class="nav-bar">
+            <a class="nav-home-link" href="./index.php">
+                <img src="./icons/home.svg" alt="Home"/>
+            </a>
 
-    <main>
-        <?php 
-            if (isset($_SESSION["userId"])) {
-                echo "<p>You are logged in!</p>";
-            } else {
-                echo "<p>You are logged out!</p>";
-            }
-        ?>
+            <section class="nav-links">
+                <ul>
+                    <li>
+                        <a href="#">OUR STORY</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                    <li>
+                        <a href="#">MENU</a>
+                    </li>
+                    <li>
+                        <a href="#">LOCATIONS</a>
+                    </li>
+                </ul>
+            </section>
 
-        <div class="image-row">
-            <div class="image-collumn">
-                <img src="./media/making-bread.png" alt="Making bread with hands" style="width:100%"/>
-                <img src="media/laughing.png" alt="Employees laughing" style="width:100%"/>
-            </div>
-            <div class="image-collumn">
-                <img src="./media/opening-bread.png" alt="Opening Bread with hands" style="width:100%"/>
-                <img src="./media/bread.png" alt="Bread" style="width:100%"/>
-            </div>
-            <div class="image-collumn">
-                <img src="./media/crossaints.png" alt="Freshly baked crossaint" style="width:100%"/>
-                <img src="./media/bagging.png" alt="Bagging up bread product" style="width:100%"/>
+            <section class="nav-actions">
+                <a class="nav-action-link" href="#">
+                    <img src="./icons/basket.svg" alt="Basket"/>
+                </a>
+                <a class="nav-action-link" href="#">
+                    <img src="./icons/search.svg" alt="Search"/>
+                </a>
+                <a class="nav-action-link" href="#">
+                    <img src="./icons/profile.svg" alt="Profile"/>
+                </a>
+            </section>
+        </nav>
+        <div class="title-logo">
+            <h1>THE MASONRY BAKERY</h1>
+            <p>THE HOME OF FRESH BAKING</p>
+        </div>
+    </div>
+    <div class="divider"></div>
+    <div class="home-background-info">
+        <div class="home-background-info-content">
+            <img src="./media/employee-working.png" alt="Employee working"/>
+            <div class="home-background-info-text-content">
+                <h1>THE MASONRY BAKERY</h1>
+                <h2>BASED IN SHROPSHIRE, ENGLAND</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <button class="our-story-button">
+                    <h2>OUR STORY</h2>
+                </button>
             </div>
         </div>
-    </main>
-
-<?php 
-    include"./includes/footer.php"
-?>
+    </div>
+    <div class="divider"></div>
+    <div class="have-a-nosey-container">
+        <a href="#">
+            <button class="have-a-nosey-button">
+                <h2>HAVE A NOSEY...</h2>
+            </button>
+        </a>
+    </div>
+    <footer>
+        <div class="divider"></div>
+        <div class="footer-content">
+            <img src="" />
+            <h2>THE MASONRY BAKERY</h2>
+        </div>
+    </footer>
+</body>
+</html>
